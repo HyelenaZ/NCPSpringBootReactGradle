35a24b693cb8c3ce30e94962c0baa4d9e34423b3
HyelenaZ <yellomouse05@gmail.com>
2024-11-15 14:35:22 +0900

    1

---
+++ Dockerfile
@@ -0,0 +1,2 @@
+FROM openjdk:17
+ARG JAR_FILE=build/libs
---
+++ src/main/frontreact/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const App = () => {
+  return (
+    <div>
+        <h1>Spring Boot + React + NCP</h1>
+        <h3>카운트 : 1</h3>
+        <img src="./image/lets.jpg" alt="공부하자" />
+    </div>
+  );
+};
+
+export default App;
